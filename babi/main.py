ce109f9818d5160c80a8e0fbcff3d3b3b53faac5
bentrevett <bentrevett@gmail.com>
2018-04-05 17:54:08 +0100

    finished main training loop with model. need to test.

--- babi/main.py
+++ babi/main.py
@@ -1,1 +1,154 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+
+import torchtext
+from torchtext.data import Field
+from torchtext.data import TabularDataset
+from torchtext.data import BucketIterator
+
+from tqdm import tqdm
+
+import models
+
+#constants 
+BATCH_SIZE = 32
+
+#define fields
+Q = Field(batch_first=True)
+A = Field(batch_first=True)
+SF = Field(batch_first=True)
+
+#{json key name: (attribute name, field name)}
+fields = {'q': ('question', Q), 
+          'a': ('answer', A), 
+          'sf0': ('sf0', SF),
+          'sf1': ('sf1', SF),
+          'sf2': ('sf2', SF),
+          'sf3': ('sf3', SF),
+          'sf4': ('sf4', SF),
+          'sf5': ('sf5', SF),
+          'sf6': ('sf6', SF),
+          'sf7': ('sf7', SF),
+          }
+
+#get data from jsonl
+train, test = TabularDataset.splits(
+                path = 'data',
+                train = 'train_all.jsonl',
+                test = 'test_all.jsonl',
+                format = 'json',
+                fields = fields
+)
+
+# print information about the data
+print('train.fields', train.fields)
+print('len(train)', len(train))
+print(vars(train[0]))
+
+# build the vocabulary
+Q.build_vocab(train.question, train.sf0, train.sf1, train.sf2, train.sf3, train.sf4, train.sf5, train.sf6, train.sf7)
+A.build_vocab(train.answer)
+SF.build_vocab(train.question, train.sf0, train.sf1, train.sf2, train.sf3, train.sf4, train.sf5, train.sf6, train.sf7)
+
+# print vocab information
+print('len(Q.vocab)', len(Q.vocab))
+print('len(SF.vocab)', len(SF.vocab))
+print('len(A.vocab)', len(A.vocab))
+print('Most common tokens', Q.vocab.freqs.most_common(10))
+
+# make iterator for splits
+train_iter, test_iter = BucketIterator.splits(
+    (train, test), 
+    batch_size=BATCH_SIZE, 
+    sort_key=lambda x: len(x.sf0),
+    device = None if torch.cuda.is_available() else -1,
+    repeat=False)
+
+#initialize model
+model = models.RelationNetwork(len(Q.vocab),
+                               len(SF.vocab),
+                               len(A.vocab))
+
+print(model)
+
+#initialize optimizer, scheduler and loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=0, verbose=True)
+
+#place on GPU
+if torch.cuda.is_available():
+    model = model.cuda()
+    criterion = criterion.cuda()
+
+best_test_loss = float('inf')
+epoch = 0
+
+while True:
+
+    epoch += 1
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for batch in tqdm(train_iter, desc='Train'):
+
+        optimizer.zero_grad()
+
+        predictions = model(batch.question, batch.sf0, batch.sf1, batch.sf2, batch.sf3, batch.sf4, batch.sf5, batch.sf6, batch.sf7)
+
+        loss = criterion(predictions, batch.answer.squeeze(1))
+
+        loss.backward()
+
+        optimizer.step()
+
+        pred = predictions.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        acc = pred.eq(batch.answer.data.view_as(pred)).long().cpu().sum()
+
+        epoch_loss += loss.data[0]
+        epoch_acc += acc/len(pred)
+
+    #calculate metrics averaged across whole batch
+    train_loss = epoch_loss / len(train_iter)
+    train_acc = epoch_acc / len(train_iter)
+
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+
+    for batch in tqdm(test_iter, desc=' Test'):
+
+        predictions = model(batch.question, batch.sf0, batch.sf1, batch.sf2, batch.sf3, batch.sf4, batch.sf5, batch.sf6, batch.sf7)
+
+        loss = criterion(predictions, batch.answer.squeeze(1))
+        
+        pred = predictions.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        acc = pred.eq(batch.answer.data.view_as(pred)).long().cpu().sum()
+
+        epoch_loss += loss.data[0]
+        epoch_acc += acc/len(pred)
+
+    #calculate metrics averaged across whole epoch
+    test_acc = epoch_acc / len(test_iter)
+    test_loss = epoch_loss / len(test_iter)
+
+    #print metrics
+    print(f'Epoch: {epoch}') 
+    print(f'Train Loss: {train_loss:.3f}, Train Acc.: {train_acc*100:.2f}%')
+    print(f'Test Loss: {test_loss:.3f}, Test Acc.: {test_acc*100:.2f}%')
+
+    if test_loss < best_test_loss:
+        best_test_loss = test_loss
+        train_patience_count = 1
+    else:
+        print(f'Losing patience... {train_patience_count}/{TRAIN_PATIENCE}')
+        train_patience_count += 1
+
+    if train_patience_count > 3:
+        print('Lost patience!')
+        sys.exit(0)
--- babi/models.py
+++ babi/models.py
@@ -1,1 +1,72 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class RelationNetwork(nn.Module):
+    def __init__(self, q_vocab_size, sf_vocab_size, a_vocab_size):
+        super().__init__()
+
+        self.q_embbeding = nn.Embedding(q_vocab_size,32)
+        self.sf_embedding = nn.Embedding(sf_vocab_size,32)
+
+        self.q_rnn = nn.LSTM(32, 32)
+        self.sf_rnn = nn.LSTM(32, 32)
+        
+        self.g1 = nn.Linear(32*3,256)
+        self.g2 = nn.Linear(256,256)
+        self.g3 = nn.Linear(256,256)
+        self.g4 = nn.Linear(256,256)
+
+        self.f1 = nn.Linear(256,256)
+        self.f2 = nn.Linear(256,512)
+        self.f3 = nn.Linear(512,a_vocab_size)
+
+    def forward(self, q, sf0, sf1, sf2, sf3, sf4, sf5, sf6, sf7):
+
+        q = self.q_embbeding(q).permute(1, 0, 2)
+        sf0 = self.sf_embedding(sf0).permute(1, 0, 2)
+        sf1 = self.sf_embedding(sf1).permute(1, 0, 2)
+        sf2 = self.sf_embedding(sf2).permute(1, 0, 2)
+        sf3 = self.sf_embedding(sf3).permute(1, 0, 2)
+        sf4 = self.sf_embedding(sf4).permute(1, 0, 2)
+        sf5 = self.sf_embedding(sf5).permute(1, 0, 2)
+        sf6 = self.sf_embedding(sf6).permute(1, 0, 2)
+        sf7 = self.sf_embedding(sf7).permute(1, 0, 2)
+
+        _, (emb_q, _) = self.q_rnn(q)
+
+        _, (emb_sf0, _) = self.sf_rnn(sf0)
+        _, (emb_sf1, _) = self.sf_rnn(sf1)
+        _, (emb_sf2, _) = self.sf_rnn(sf2)
+        _, (emb_sf3, _) = self.sf_rnn(sf3)
+        _, (emb_sf4, _) = self.sf_rnn(sf4)
+        _, (emb_sf5, _) = self.sf_rnn(sf5)
+        _, (emb_sf6, _) = self.sf_rnn(sf6)
+        _, (emb_sf7, _) = self.sf_rnn(sf7)
+
+        emb_sfs = [emb_sf0, emb_sf1, emb_sf2, emb_sf3, emb_sf4, emb_sf5, emb_sf6, emb_sf7]
+
+        g_o = Variable(torch.zeros(32,256))
+
+        if torch.cuda.is_available():
+            g_o = g_o.cuda()
+
+        for i, _ in enumerate(emb_sfs):
+            o_i = emb_sfs[i]
+            for j, _ in enumerate(emb_sfs):
+                o_j = emb_sfs[j]
+                #print(o_i.shape)
+                #print(o_j.shape)
+                #print(emb_q.shape)
+                x = F.relu(self.g1(torch.cat((o_i,o_j,emb_q),2)))
+                x = F.relu(self.g2(x))
+                x = F.relu(self.g3(x))
+                x = F.relu(self.g4(x))
+                g_o.add(x)
+
+        x = F.relu(self.f1(g_o))
+        x = F.relu(self.f2(x))
+        x = self.f3(x)
+
+        return x
